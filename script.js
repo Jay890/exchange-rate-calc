fa525015eba94a77898fa28eedc1d75dfd2a6c54
Jason Leong <vergil_dmc1@hotmail.com>
2021-05-02 15:51:15 +1000

    Covid 19 data
    Simple website using covid 19 API to retrieve live data
    and display it in a table
    Using Jquery

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-// sample request to request to json file
+const currency_one = document.getElementById('currency-one');
+const currency_two = document.getElementById('currency-two');
+const amount_one = document.getElementById('amount-one');
+const amount_two = document.getElementById('amount_two');
+const rate = document.getElementById('rate');
+const swap = document.getElementById('swap');
 
+// fetch exchange rates and update the DOM
 function calculate() {
-  /* fetch is built into the browser
-   getrequest
-   runs asynchronously (in the background and when its done fetching it returns a promise)  fetch('items.json');.
-   catch that promise with .then()
-   */
-  fetch('items.json').then((res) => {
-    res
-      .json()
-      //second promise
-      .then((data) => console.log(data));
-  });
+  const curr_one = currency_one.value;
+  const curr_two = currency_two.value;
+  console.log(curr_one, curr_two);
+  fetch(`https://api.exchangerate-api.com/v4/latest/${curr_one}`)
+    .then((res) => res.json)
+    .then((data) => {
+      // console.log(data);
+      const rate_number = data.rates[curr_two];
+      // console.log(rate);
+      rate.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+      amount_two.value = (amount_two * rate).toFixed(2);
+    });
 }
+
+// Event Listeners
+currency_one.addEventListener('change', calculate);
+currency_two.addEventListener('change', calculate);
+amount_one.addEventListener('input', calculate);
+amount_two.addEventListener('input', calculate);
+
+swap.addEventListener('click', () => {
+  const temp = currency_one.value;
+  currency_one = currency_two;
+  currency_two = temp;
+  calculate();
+});
+
 calculate();
